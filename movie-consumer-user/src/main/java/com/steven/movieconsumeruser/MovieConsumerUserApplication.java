9178c1928af5e85f4d5a0d4cf6864651b9e1cc1d
zhaojie <zhaojie@wafersystems.com>
2018-09-21 16:48:08 +0800

    将电影的微服务注册在Eureka中

--- movie-consumer-user/src/main/java/com/steven/movieconsumeruser/MovieConsumerUserApplication.java
+++ movie-consumer-user/src/main/java/com/steven/movieconsumeruser/MovieConsumerUserApplication.java
@@ -2,6 +2,7 @@ package com.steven.movieconsumeruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
  * @author Steven
  * @date 2018年9月21日 15:56:54
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class MovieConsumerUserApplication {
 
--- movie-provider-user/src/main/java/com/steven/movieprovideruser/MovieProviderUserApplication.java
+++ movie-provider-user/src/main/java/com/steven/movieprovideruser/MovieProviderUserApplication.java
@@ -2,11 +2,13 @@ package com.steven.movieprovideruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Steven
  * @since 2018年9月21日 15:00:44
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class MovieProviderUserApplication {
 
