6ae590337cde97bfa3347a889080385d3d97f00d
zhaojie <zhaojie@wafersystems.com>
2018-09-25 18:49:00 +0800

    添加Ribbon实现客户端负载均衡

--- movie-consumer-user/src/main/java/com/steven/movieconsumeruser/MovieConsumerUserApplication.java
+++ movie-consumer-user/src/main/java/com/steven/movieconsumeruser/MovieConsumerUserApplication.java
@@ -2,11 +2,14 @@ package com.steven.movieconsumeruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
+ * 1.添加@LoadBalanced就可以开启Ribbon负载均衡
+ *
  * @author Steven
  * @date 2018年9月21日 15:56:54
  */
@@ -15,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 public class MovieConsumerUserApplication {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- movie-consumer-user/src/main/java/com/steven/movieconsumeruser/controller/UserController.java
+++ movie-consumer-user/src/main/java/com/steven/movieconsumeruser/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.steven.movieconsumeruser.controller;
 
 import com.steven.movieconsumeruser.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,7 @@ import java.util.List;
  * @author Steven
  * @since 2018年9月21日 14:57:24
  */
+@Slf4j
 @RestController
 public class UserController {
 
@@ -24,6 +27,9 @@ public class UserController {
 
     @Autowired
     private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
@@ -36,4 +42,11 @@ public class UserController {
     public List<ServiceInstance> showInfo() {
         return discoveryClient.getInstances("movie-provider-user");
     }
+
+    @GetMapping("/log-instance")
+    public void logUserInstance() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("movie-provider-user");
+        log.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
+    }
+
 }
