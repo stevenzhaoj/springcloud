a9025d554d5495fae8687b9e948049dbcb8b4a6f
zhaojie <zhaojie@wafersystems.com>
2018-09-25 16:10:48 +0800

    微服务添加元数据和访问元数据

--- movie-consumer-user/src/main/java/com/steven/movieconsumeruser/controller/UserController.java
+++ movie-consumer-user/src/main/java/com/steven/movieconsumeruser/controller/UserController.java
@@ -2,11 +2,15 @@ package com.steven.movieconsumeruser.controller;
 
 import com.steven.movieconsumeruser.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author Steven
@@ -18,10 +22,18 @@ public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
         ResponseEntity<User> response =
             restTemplate.getForEntity("http://localhost:8000/user/" + id, User.class);
         return response.getBody();
+    }
+
+    @GetMapping("/user-instance")
+    public List<ServiceInstance> showInfo() {
+        return discoveryClient.getInstances("movie-provider-user");
     }
 }
